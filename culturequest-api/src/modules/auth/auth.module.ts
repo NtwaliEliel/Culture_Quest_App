a25f5ed9e1e285ec330d93bc38595a5ce262cf0b
nshimiyeemmy <nshimiyee311@gmail.com>
2025-07-17 17:58:08 +0200

    fix: local strategy issue

--- culturequest-api/src/modules/auth/auth.module.ts
+++ culturequest-api/src/modules/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from '../users/users.module';
 
 @Module({
@@ -24,7 +23,7 @@ import { UsersModule } from '../users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, LocalStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {} 
